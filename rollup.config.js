3273ce6c40bcde05877729cf58c97cb0539cd8c4
qiunanya <1766226354@qq.com>
2024-02-07 11:35:44 +0800

    完成父类追加加载动画功能

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
 import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
 import { readFileSync } from "fs";
 const packageJson = JSON.parse(readFileSync("./package.json", "utf8"));
 const pkgName = packageJson.umdModuleName;
@@ -11,24 +12,21 @@ export default {
     input: "./src/index.ts", // 配置打包入口
     output: [
         {
-            // 支持es module
+            // 支持esm module
             file: "dist/index.esm.js",
             format: "esm",
-            exports: 'auto'
         },
         {
             // 支持node环境
             file: "dist/index.cjs.js",
             format: "cjs",
-            exports: 'auto'
         },
         {
             // 支持浏览器
             file: "dist/index.js", // 最终打包出来的文件路径和文件名
             format: "umd", // umd是兼容amd/cjs/iife的通用打包格式，适合浏览器
             name: pkgName,
-            exports: 'auto'
-        }
+        },
     ],
     plugins: [
         commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
@@ -36,7 +34,12 @@ export default {
         json(),
         terser(), // 压缩代码插件配置
         typescript({
-            "tsconfig": "./tsconfig.json"
+            tsconfig: "./tsconfig.json",
         }),
+        copy({
+            targets: [
+                { src: 'src/theme/**/*', dest: 'dist/theme'}
+            ]
+        })
     ],
 };
--- src/load/index.ts
+++ src/load/index.ts
@@ -5,18 +5,22 @@ import { createCircleElement, createSvg } from './tool';
 
 interface Options {
     duration: number,
-    name: string
+    name: string,
+    parent: string
 }
 class Loading {
     name: string = 'svg';
     duration: number = 300;
+    parent: string = 'body';
     private container: HTMLElement;
     private loader: HTMLElement;
     private svgLoader: SVGElement;
     private svgLoaderIcon: SVGElement;
+    private parentDom?: HTMLElement;
     constructor(Opt: Options) {
         this.name = Opt?.name
         this.duration = Opt?.duration
+        this.parent = Opt?.parent || 'body'
         this.container = document.createElement('DIV')
         this.container.classList.add('esay-loading-wrap')
         this.svgLoaderIcon = createCircleElement({r: 0 })
@@ -25,19 +29,33 @@ class Loading {
     }
     public start () {
         this.container.appendChild(this.svgLoader)
-        document.body.appendChild(this.container);
-        const { height, width, x, y } = this.svgLoader.getBoundingClientRect()
+        try {
+            if (this.parent !== 'body') {
+                this.parentDom = document.querySelector(`.${this.parent}`) as HTMLElement;
+                this.parentDom?.appendChild(this.container)
+            } else {
+                this.container.style.position = 'fixed'
+                document.body.appendChild(this.container)
+            }
+        } catch (error) {
+            console.log(error);
+        }
+        console.log(this.parent, 11111);
         const arr = this.svgLoader.getAttribute('viewBox')?.split(' ').map(el => +el)
         this.svgLoaderIcon = createCircleElement({
             r: 20,
             cy: arr?arr[2]/2: 100,
             cx: arr?arr[2]/2:100
         })
-        console.log(arr, this.svgLoader.getAttribute('viewBox'), 'viewBox');
+        
         this.svgLoader.appendChild(this.svgLoaderIcon)
     }
     public close () {
-        document.body.removeChild(this.container);
+        if (this.parentDom && this.parent !== 'body') {
+            this.parentDom.removeChild(this.container)
+        } else {
+            document.body.removeChild(this.container);
+        }
     }
 }
 
